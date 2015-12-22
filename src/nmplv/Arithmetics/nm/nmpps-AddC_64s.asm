632ca487f1481d84f2d147a65e7a9eb7256ffcf6
mushkaev <mushkaev@module.ru>
2015-12-22 14:33:58 +0300

    And

---
+++ src/nmplv/Arithmetics/nm/nmpps-AddC_64s.asm
@@ -0,0 +1,67 @@
+//------------------------------------------------------------------------
+//
+//  $Workfile:: AddVN.as $
+//
+//  Векторно-матричная библиотека
+//
+//  Copyright (c) RC Module Inc.
+//
+//  $Revision: 1.1 $      $Date: 2004/11/22 13:50:03 $
+//
+//! \if file_doc
+//!
+//! \file   AddVN.asm
+//! \author Сергей Мушкаев
+//! \brief  Функции суммирования для векторов.
+//!
+//! \endif
+//!
+//------------------------------------------------------------------------
+
+extern vec_data_add_ram:label;
+extern _nmppsTmpBuffer16_G_:long;
+
+begin ".text_nmplv"
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//! \fn void nmppsAddC_64s (nm64s *pSrcVec, int64b nVal, nm64s *pDstVec, int nSize) 
+//!
+//! \perfinclude _nmppsAddC_64s.html
+
+global _nmppsAddC_64s:label;
+
+<_nmppsAddC_64s>
+.branch;
+	ar5 = sp - 2;
+
+	push ar0,gr0;
+	push ar1,gr1;
+	push ar5,gr5;
+	push ar6,gr6;
+
+	ar0 = [--ar5];	// SrcBuff
+	ar1 = ar5-3;
+	ar5 = ar1;
+	//ar1 = [--ar5];	// Increment*
+	ar6 = [--ar5];	// pDstVec
+	gr5 = [--ar5];	// nSize
+
+	nb1 = 00000000h;	// nSize in 64-bit longs
+	wtw;
+
+	gr0 = 2;
+	delayed call vec_data_add_ram;
+	gr6 = 2;
+	
+	pop ar6,gr6;
+	pop ar5,gr5;
+	pop ar1,gr1;
+	pop ar0,gr0;
+return with gr7=false;
+.wait;
+
+
+
+end ".text_nmplv";
---
+++ src/nmplv/Bitwise/pc/nmppsAndC.c
@@ -0,0 +1,85 @@
+#include "nmpp.h"
+
+///////////////////////////////////////////////////////////////////////////////
+// pDstVec[i] = pSrcVec[i] & Mask 
+void nmppsAndC_p64u(nm64u*	Src1,		// Input Buffer
+	nm64u*	Mask,		// Input mask
+	nm64u*	pDstVec,		// Output Buffer
+	int		nSize		// size of input buffer in 64 bit elements. nSize={1,2,..,n}
+	)
+{
+	#ifdef RPC
+	//RPC_HOST_PPI("nmppsAndC_64u",pSrcVec,pDstVec,nSize,8);
+	#else
+
+
+int i;
+	for (i=0; i<nSize; i++)
+		pDstVec[i] = Src1[i] & Mask[0];
+
+	#endif
+}
+
+void nmppsAndC_64u(
+	nm64u*	Src,		// Input Buffer
+	uint64b	Mask,		// Input mask
+	nm64u*	Dst,		// Output Buffer
+	int		Size		// size of input buffer in 64 bit elements. nSize={1,2,..,n}
+	)
+{
+	#ifdef RPC
+	RPC_HOST_PLPI("nmppsAndC_64u",Src,Mask,Dst,Size,8);
+	#else
+	int i;
+	for (i=0; i<Size; i++)
+		Dst[i] = Src[i] & Mask;
+	#endif
+}
+
+void nmppsAndC_32u(
+	nm32u*	Src,		// Input Buffer
+	uint32b	Mask,		// Input mask
+	nm32u*	Dst,		// Output Buffer
+	int		Size		// size of input buffer in 3 bit elements. nSize={2,4..,n}
+	)
+{
+	#ifdef RPC
+	RPC_HOST_PIPI("nmppsAndC_32u",Src,Mask,Dst,Size,4);
+	#else
+	int i;
+	for (i=0; i<Size; i++)
+		Dst[i] = Src[i] & Mask;
+	#endif
+}
+
+void nmppsAndC_16u(
+	nm16u*	Src,		// Input Buffer
+	uint16b	Mask,		// Input mask
+	nm16u*	Dst,		// Output Buffer
+	int		Size		// size of input buffer in 3 bit elements. nSize={2,4..,n}
+	)
+{
+	#ifdef RPC
+	RPC_HOST_PIPI("nmppsAndC_16u",Src,Mask,Dst,Size,2);
+	#else
+	int i;
+	for (i=0; i<Size; i++)
+		Dst[i] = Src[i] & Mask;
+	#endif
+}
+
+void nmppsAndC_8u(
+	nm8u*	Src,		// Input Buffer
+	uint8b	Mask,		// Input mask
+	nm8u*	Dst,		// Output Buffer
+	int		Size		// size of input buffer in 3 bit elements. nSize={2,4..,n}
+	)
+{
+	#ifdef RPC
+	RPC_HOST_PIPI("nmppsAndC_8u",Src,Mask,Dst,Size,1);
+	#else
+	int i;
+	for (i=0; i<Size; i++)
+		Dst[i] = Src[i] & Mask;
+	#endif
+}

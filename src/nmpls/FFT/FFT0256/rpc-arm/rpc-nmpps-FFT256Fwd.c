0ce7ca25b937b97e15a8c09ebc8b63db03b8fd50
mushkaev <mushkaev@module.ru>
2016-06-30 14:02:26 +0300

    1

--- src/nmpls/FFT/FFT0256/rpc-arm/rpc-nmpps-FFT256Fwd.c
+++ src/nmpls/FFT/FFT0256/rpc-arm/rpc-nmpps-FFT256Fwd.c
@@ -1,9 +1,12 @@
-#include "malloc32.h"
 #include "fft2.h"
 #include "rpc-host.h"	
 
-	void nmppsFFT256Inv(const nm32sc* src, nm32sc* dst,const  NmppsFFTSpec* spec)
+
+
+	void nmppsFFT256Fwd(const nm32sc* src, nm32sc* dst,const  NmppsFFTSpec* spec)
 	{
+		clock_t t0,t1,t2,t3;
+		t0=clock();
 		int ret;	
 		int size=256;
 		int k=8;
@@ -11,35 +14,38 @@
 		struct aura_buffer *iobuf_dst = aura_buffer_request(n, size*k);	
 		memcpy(iobuf_src->data,src,size*k);	
 		struct aura_buffer *retbuf; 
-		ret = aura_call(n, "nmppsFFT256Inv", &retbuf,  iobuf_src, iobuf_dst, spec); 
+		t1=clock();
+		ret = aura_call_raw(n, 2, &retbuf,  iobuf_src, iobuf_dst, spec); 
+		t2=clock();
 		if (ret != 0) {
-			slog(0, SLOG_ERROR, "call nmppsFFT256Inv  failed, reason: %d\n", ret);
-			BUG(n, "Call nmppsFFT256Inv failed!"); 
+			slog(0, SLOG_ERROR, "call failed, reason: %d\n", ret);
+			BUG(n, "Call nmppsFFT256Fwd failed!"); 
 		}
 		memcpy(dst,iobuf_dst->data,size*k);
 		aura_buffer_release( iobuf_dst); 
 		aura_buffer_release( iobuf_src); 
 		aura_buffer_release( retbuf); 
-		slog(0, SLOG_INFO, "ARM: Call nmppsFFT256Inv -ok"); 
+		t3=clock();
+		slog(3, SLOG_INFO, "ARM: Call nmppsFFT256Fwd -ok"); 
+
 	}
 
-
-	int nmppsFFT256InvInitAlloc( NmppsFFTSpec** spec, const void* src, const void* dst, int settings)
+	int nmppsFFT256FwdInitAlloc( NmppsFFTSpec** spec, const void* src, const void* dst,  int settings)
 	{
 		struct aura_buffer *iobuf_src = aura_buffer_request(n, 256*8);	
 		struct aura_buffer *iobuf_dst = aura_buffer_request(n, 256*8);	
 		struct aura_buffer *retbuf; 
-		int ret =  aura_call(n, "nmppsFFT256InvInitAlloc", &retbuf,  iobuf_src, iobuf_dst, settings); 
+		int ret =  aura_call(n, "nmppsFFT256FwdInitAlloc", &retbuf,  iobuf_src, iobuf_dst, settings); 
 		if (ret != 0) {
 			slog(0, SLOG_ERROR, "call failed, reason: %d\n", ret);
-			BUG(n, "Call nmppsFFT256InvInitAlloc failed!"); 
+			BUG(n, "Call nmppsFFT256FwdInitAlloc failed!"); 
 		}
 		*spec = (NmppsFFTSpec*) aura_buffer_get_u32(retbuf);
 		ret   = aura_buffer_get_u32(retbuf);
 		aura_buffer_release( iobuf_src); 
 		aura_buffer_release( iobuf_dst); 
 		aura_buffer_release( retbuf); 
-		slog(0, SLOG_INFO, "ARM: Call nmppsFFT256InvInitAlloc -ok"); 
+		slog(3, SLOG_INFO, "ARM: Call nmppsFFT256FwdInitAlloc -ok"); 
 		return ret;
 	}
-	
+

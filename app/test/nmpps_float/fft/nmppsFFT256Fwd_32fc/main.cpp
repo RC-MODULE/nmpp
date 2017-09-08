cd6a06ddf00f89cdce48312560e2fa9d69c67260
Andrey Brodyazhenko <a.brodyazhenko@module.ru>
2017-09-08 16:53:20 +0300

    Added doxy description

--- app/test/nmpps_float/fft/nmppsFFT256Fwd_32fc/main.cpp
+++ app/test/nmpps_float/fft/nmppsFFT256Fwd_32fc/main.cpp
@@ -3,16 +3,17 @@
 #include "time.h"
 #include "fft_32fc.h"
 #include "nmpp.h"
-//#include <stdio.h>
+#include <stdio.h>
 
 int main()
 {
-	int i, tm;
+	int i, tm, time;
 	nm32fcr *src, *dst;
+	clock_t t1, t2;
 	// best config (tm = 2155, src - malloc, dst - malloc2, or
 	// src - malloc3, dst - malloc1)
 	src = (nm32fcr *)malloc(256  * sizeof(nm32fcr));
-	dst = (nm32fcr *)malloc(256  * sizeof(nm32fcr));
+	dst = (nm32fcr *)malloc1(256  * sizeof(nm32fcr));
 	for(i = 0; i < 256; i++) {
 		src[i].im = 0;
 		src[i].re = i;
@@ -24,7 +25,10 @@ int main()
 	if(tm == 123) {
 		return 123;
 	}
+	t1 = clock();
 	nmppsFFT256Fwd_32fc(src, dst, rat);
+	t2 = clock();
+	time = t2 - t1;
 	tm = nmppsFFTFree_32fc(rat);
 	unsigned int crc1 = 0;
 	unsigned int crc2 = 0;
@@ -38,5 +42,6 @@ int main()
 	nmppsCrcAcc_32f((nm32f *)dst, 16, 256*2, &crc2);
 	// printf("%d\n", crc1>>2);
 	// printf("%d\n", crc2>>2);
+	printf("%d\n", time);
 	return (crc1+crc2)>>2;
 }
--- include/fft_32fc.h
+++ include/fft_32fc.h
@@ -18,51 +18,155 @@ typedef struct
     int order;
 } NmppsFFTSpec_32fc;
 
-void nmppsDFT8Fwd_32fc(nm32fcr *x8, nm32fcr *X8, NmppsFFTSpec_32fc *spec8);
-void nmppsFFT16Fwd_32fc(nm32fcr *x16, nm32fcr *X16, NmppsFFTSpec_32fc *spec16);
-void nmppsFFT32Fwd_32fc(nm32fcr *x32, nm32fcr *X32, NmppsFFTSpec_32fc *spec32);
-void nmppsFFT64Fwd_32fc(nm32fcr *x64, nm32fcr *X64, NmppsFFTSpec_32fc *spec64);
-void nmppsFFT128Fwd_32fc(nm32fcr *x128, nm32fcr *X128, NmppsFFTSpec_32fc *spec128);
-void nmppsFFT256Fwd_32fc(nm32fcr *x256, nm32fcr *X256, NmppsFFTSpec_32fc *spec256);
-void nmppsFFT512Fwd_32fc(nm32fcr *x512, nm32fcr *X512, NmppsFFTSpec_32fc *spec512);
-void nmppsFFT1024Fwd_32fc(nm32fcr *x1024, nm32fcr *X1024, NmppsFFTSpec_32fc *spec1024);
-void nmppsFFT2048Fwd_32fc(nm32fcr *x2048, nm32fcr *X2048, NmppsFFTSpec_32fc *spec2048);
+typedef struct
+{
+  NmppsFFTSpec_32fc *specX;
+  NmppsFFTSpec_32fc *specY;
+} NmppiFFTSpec_32fc;
 
-void nmppsDFT8Inv_32fc(nm32fcr *ix8, nm32fcr *iX8, NmppsFFTSpec_32fc *ispec8);
-void nmppsFFT16Inv_32fc(nm32fcr *ix16, nm32fcr *iX16, NmppsFFTSpec_32fc *ispec16);
-void nmppsFFT32Inv_32fc(nm32fcr *ix32, nm32fcr *iX32, NmppsFFTSpec_32fc *ispec32);
-void nmppsFFT64Inv_32fc(nm32fcr *ix64, nm32fcr *iX64, NmppsFFTSpec_32fc *ispec64);
-void nmppsFFT128Inv_32fc(nm32fcr *ix128, nm32fcr *iX128, NmppsFFTSpec_32fc *ispec128);
-void nmppsFFT256Inv_32fc(nm32fcr *ix256, nm32fcr *iX256, NmppsFFTSpec_32fc *ispec256);
-void nmppsFFT512Inv_32fc(nm32fcr *ix512, nm32fcr *iX512, NmppsFFTSpec_32fc *ispec512);
-void nmppsFFT1024Inv_32fc(nm32fcr *ix1024, nm32fcr *iX1024, NmppsFFTSpec_32fc *ispec1024);
-void nmppsFFT2048Inv_32fc(nm32fcr *ix2048, nm32fcr *iX2048, NmppsFFTSpec_32fc *ispec2048);
+// sFFT_32fc
 
+
+    /**
+    \defgroup sFFT_Fwd FFTFwd_32fc
+    \ingroup sFFT_32fc
+   	\brief Функции для вычисления прямого БПФ с плавающей точкой на векторами комплексных чисел
+	
+	\param [in] x входной вектор комплексных чисел (на мнимая и действительная части имеют тип float)
+	\retval [out] X выходной вектор комплексных чисел (на мнимая и действительная части имеют тип float)
+	\param [in] spec структра, содержащая необходимые коэффициенты, для вычисления прямого БПФ определенного размера
+    */
+    //! \{
+void nmppsDFT8Fwd_32fc(nm32fcr *x, nm32fcr *X, NmppsFFTSpec_32fc *spec);
+void nmppsFFT16Fwd_32fc(nm32fcr *x, nm32fcr *X, NmppsFFTSpec_32fc *spec);
+void nmppsFFT32Fwd_32fc(nm32fcr *x, nm32fcr *X, NmppsFFTSpec_32fc *spec);
+void nmppsFFT64Fwd_32fc(nm32fcr *x, nm32fcr *X, NmppsFFTSpec_32fc *spec);
+void nmppsFFT128Fwd_32fc(nm32fcr *x, nm32fcr *X, NmppsFFTSpec_32fc *spec);
+void nmppsFFT256Fwd_32fc(nm32fcr *x, nm32fcr *X, NmppsFFTSpec_32fc *spec);
+void nmppsFFT512Fwd_32fc(nm32fcr *x, nm32fcr *X, NmppsFFTSpec_32fc *spec);
+void nmppsFFT1024Fwd_32fc(nm32fcr *x, nm32fcr *X, NmppsFFTSpec_32fc *spec);
+void nmppsFFT2048Fwd_32fc(nm32fcr *x, nm32fcr *X, NmppsFFTSpec_32fc *spec);
+    //! \}
+	
+
+
+	
+    /**
+    \defgroup sFFT_Fwd FFTInv
+    \ingroup sFFT_32fc
+   	\brief Функции для вычисления обратного БПФ с плавающей точкой на векторами комплексных чисел
+	
+	\param [in] x входной вектор комплексных чисел (на мнимая и действительная части имеют тип float)
+	\retval [out] X выходной вектор комплексных чисел (на мнимая и действительная части имеют тип float)
+	\param [in] ispec структра, содержащая необходимые коэффициенты, для вычисления обратного БПФ определенного размера
+    */
+    //! \{
+void nmppsDFT8Inv_32fc(nm32fcr *ix, nm32fcr *iX, NmppsFFTSpec_32fc *ispec);
+void nmppsFFT16Inv_32fc(nm32fcr *ix, nm32fcr *iX, NmppsFFTSpec_32fc *ispec);
+void nmppsFFT32Inv_32fc(nm32fcr *ix, nm32fcr *iX, NmppsFFTSpec_32fc *ispec);
+void nmppsFFT64Inv_32fc(nm32fcr *ix, nm32fcr *iX, NmppsFFTSpec_32fc *ispec);
+void nmppsFFT128Inv_32fc(nm32fcr *ix, nm32fcr *iX, NmppsFFTSpec_32fc *ispec);
+void nmppsFFT256Inv_32fc(nm32fcr *ix, nm32fcr *iX, NmppsFFTSpec_32fc *ispec);
+void nmppsFFT512Inv_32fc(nm32fcr *ix, nm32fcr *iX, NmppsFFTSpec_32fc *ispec);
+void nmppsFFT1024Inv_32fc(nm32fcr *ix, nm32fcr *iX, NmppsFFTSpec_32fc *ispec);
+void nmppsFFT2048Inv_32fc(nm32fcr *ix, nm32fcr *iX, NmppsFFTSpec_32fc *ispec);
+    //! \}	
+
+/**
+ *  \ingroup sFFT_32fc
+ *  \brief Функция для вычисления прямого БПФ с плавающей точкой на векторами комплексных чисел
+ *  
+ *  \param [in] x входной вектор комплексных чисел (на мнимая и действительная части имеют тип float)
+ *  \param [in] X выходной вектор комплексных чисел (на мнимая и действительная части имеют тип float)
+ *  \param [in] Spc структра, содержащая необходимые коэффициенты, для вычисления прямого БПФ определенного размера
+ *  \return Функция возвращают 0 в случае успешной инициализации и отрицательное число (от -1 и меньше) в случае ошибок
+ * 
+ */
 int nmppsFFTFwd_32fc(nm32fcr *x, nm32fcr *X, NmppsFFTSpec_32fc *Spc);
+
+/**
+ *  \ingroup sFFT_32fc
+ *  \brief Функция для вычисления обратного БПФ с плавающей точкой на векторами комплексных чисел
+ *  
+ *  \param [in] ix входной вектор комплексных чисел (на мнимая и действительная части имеют тип float)
+ *  \param [in] iX выходной вектор комплексных чисел (на мнимая и действительная части имеют тип float)
+ *  \param [in] iSpc структра, содержащая необходимые коэффициенты, для вычисления обратного БПФ определенного размера
+ *  \return Функция возвращают 0 в случае успешной инициализации и отрицательное число (от -1 и меньше) в случае ошибок
+ * 
+ */
 int nmppsFFTInv_32fc(nm32fcr *ix, nm32fcr *iX, NmppsFFTSpec_32fc *iSpc);
 
-int nmppsFFT16InvInitAlloc_32fc(NmppsFFTSpec_32fc **iaddr16);
-int nmppsFFT32InvInitAlloc_32fc(NmppsFFTSpec_32fc **iaddr32);
-int nmppsFFT64InvInitAlloc_32fc(NmppsFFTSpec_32fc **iaddr64);
-int nmppsFFT128InvInitAlloc_32fc(NmppsFFTSpec_32fc **iaddr128);
-int nmppsFFT256InvInitAlloc_32fc(NmppsFFTSpec_32fc **iaddr256);
-int nmppsFFT512InvInitAlloc_32fc(NmppsFFTSpec_32fc **iaddr512);
-int nmppsFFT1024InvInitAlloc_32fc(NmppsFFTSpec_32fc **iaddr1024);
-int nmppsFFT2048InvInitAlloc_32fc(NmppsFFTSpec_32fc **iaddr2048);
 
-int nmppsFFT16FwdInitAlloc_32fc(NmppsFFTSpec_32fc **addr16);
-int nmppsFFT32FwdInitAlloc_32fc(NmppsFFTSpec_32fc **addr32);
-int nmppsFFT64FwdInitAlloc_32fc(NmppsFFTSpec_32fc **addr64);
-int nmppsFFT128FwdInitAlloc_32fc(NmppsFFTSpec_32fc **addr128);
-int nmppsFFT256FwdInitAlloc_32fc(NmppsFFTSpec_32fc **addr256);
-int nmppsFFT512FwdInitAlloc_32fc(NmppsFFTSpec_32fc **addr512);
-int nmppsFFT1024FwdInitAlloc_32fc(NmppsFFTSpec_32fc **addr1024);
-int nmppsFFT2048FwdInitAlloc_32fc(NmppsFFTSpec_32fc **addr2048);
+    /**
+    \defgroup sFFT_Fwd FFTInvInitAlloc
+    \ingroup sFFT_32fc
+   	\brief Функции инициализации структур коэффициентов, необходимых для вычисления обратного БПФ определенного размера
+	
+	\param [in] iaddr двойной указатель на структуру коэффициентов
+	\return Функции возвращают 0 в случае успешной инициализации и отрицательное число (от -1 и меньше) в случае ошибок
+    */
+    //! \{
+int nmppsFFT16InvInitAlloc_32fc(NmppsFFTSpec_32fc **iaddr);
+int nmppsFFT32InvInitAlloc_32fc(NmppsFFTSpec_32fc **iaddr);
+int nmppsFFT64InvInitAlloc_32fc(NmppsFFTSpec_32fc **iaddr);
+int nmppsFFT128InvInitAlloc_32fc(NmppsFFTSpec_32fc **iaddr);
+int nmppsFFT256InvInitAlloc_32fc(NmppsFFTSpec_32fc **iaddr);
+int nmppsFFT512InvInitAlloc_32fc(NmppsFFTSpec_32fc **iaddr);
+int nmppsFFT1024InvInitAlloc_32fc(NmppsFFTSpec_32fc **iaddr);
+int nmppsFFT2048InvInitAlloc_32fc(NmppsFFTSpec_32fc **iaddr);
+    //! \}	
 
+
+    /**
+    \defgroup sFFT_Fwd FFTFwdInitAlloc
+    \ingroup sFFT_32fc
+   	\brief Функции инициализации структур коэффициентов, необходимых для вычисления прямого БПФ определенного размера
+	
+	\param [in] addr двойной указатель на структуру коэффициентов
+	\return Функции возвращают 0 в случае успешной инициализации и отрицательное число (от -1 и меньше) в случае ошибок
+    */
+    //! \{
+int nmppsFFT16FwdInitAlloc_32fc(NmppsFFTSpec_32fc **addr);
+int nmppsFFT32FwdInitAlloc_32fc(NmppsFFTSpec_32fc **addr);
+int nmppsFFT64FwdInitAlloc_32fc(NmppsFFTSpec_32fc **addr);
+int nmppsFFT128FwdInitAlloc_32fc(NmppsFFTSpec_32fc **addr);
+int nmppsFFT256FwdInitAlloc_32fc(NmppsFFTSpec_32fc **addr);
+int nmppsFFT512FwdInitAlloc_32fc(NmppsFFTSpec_32fc **addr);
+int nmppsFFT1024FwdInitAlloc_32fc(NmppsFFTSpec_32fc **addr);
+int nmppsFFT2048FwdInitAlloc_32fc(NmppsFFTSpec_32fc **addr);
+    //! \}	
+
+/**
+ *  \ingroup sFFT_32fc
+ *  \brief Функция инициализации структур коэффициентов, необходимых для вычисления прямого БПФ определенного размера
+ *  
+ *  \param [in] Spec двойной указатель на структуру коэффициентов
+ *  \param [in] Order размерность БПФ, которое нужно вычислить, например, для БПФ256 этот параметр равен 8 (т.к. 2^8 = 256)
+ *  \return Функция возвращают 0 в случае успешной инициализации и отрицательное число (от -1 и меньше) в случае ошибок
+ *  \details 
+ */
 int nmppsFFTFwdInitAlloc_32fc(NmppsFFTSpec_32fc **Spec, int Order);
+
+/**
+ *  \ingroup sFFT_32fc
+ *  \brief Функция инициализации структур коэффициентов, необходимых для вычисления обратного БПФ определенного размера
+ *  
+ *  \param [in] iSpec двойной указатель на структуру коэффициентов
+ *  \param [in] iOrder размерность БПФ, которое нужно вычислить, например, для БПФ256 этот параметр равен 8 (т.к. 2^8 = 256)
+ *  \return Функция возвращают 0 в случае успешной инициализации и отрицательное число (от -1 и меньше) в случае ошибок
+ *  \details 
+ */
 int nmppsFFTInvInitAlloc_32fc(NmppsFFTSpec_32fc **iSpec, int iOrder);
 
-int nmppsFFTFree_32fc(NmppsFFTSpec_32fc *specN);
+
+/**
+ *  \ingroup sFFT_32fc
+ *  \brief Функция освобождает память, выделенную под коэффициенты, хранящиеся в структуре spec
+ *  
+ *  \param [in] spec указатель на структуру коэффициентов
+ *  \return возвращает 0 в случае успешного освобождения памяти и -1 в случае ошибки
+ */
+int nmppsFFTFree_32fc(NmppsFFTSpec_32fc *spec);
 
 #ifdef __cplusplus
 		};

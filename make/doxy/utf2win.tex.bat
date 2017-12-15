b58a207fe8fddc46df61be53ae9629b1352a0833
Sergey Mushkaev <mushkaev@module.ru>
2017-12-15 23:14:29 +0300

    pdf ++

--- make/doxy/utf2win.tex.bat
+++ make/doxy/utf2win.tex.bat
@@ -1,25 +1,27 @@
 @rm -f .utf2win
 @for %%i in (latex\*.tex) do (
-	@rm -f temp.txt 
+	@rm -f temp.txt
 	iconv -f utf-8 -t cp1251 %%i >  temp.txt
 	cp temp.txt %%i
 	@if ERRORLEVEL 1 goto WTF 
 )
 
 @for %%i in (latex\*.sty) do (
-	@rm -f temp.txt 
+	@rm -f temp.txt
 	iconv -f utf-8 -t cp1251 %%i >  temp.txt
 	cp temp.txt %%i
 	@if ERRORLEVEL 1 goto WTF 
 )
 
 @for %%i in (latex\*.eps) do (
-	@rm -f temp.txt 
+	@rm -f temp.txt
 	iconv -f utf-8 -t cp1251 %%i >  temp.txt
 	cp temp.txt %%i
 	@if ERRORLEVEL 1 goto WTF 
 )
 
+
+sed -e "s/usepackage\[utf8\]/usepackage[cp1251]/g" latex/refman.tex -i
 
 @echo  "utf-8 to windows-1251 converted" > .cp-win1251.tex
 @exit 0

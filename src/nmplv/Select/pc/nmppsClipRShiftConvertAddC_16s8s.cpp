9d9372ba5a17e8e5e37192d08f6cb465e09e44e4
Andrey Brodyazhenko <brodyazhenko@gmail.com>
2018-03-01 15:09:55 +0300

    nmppsClipRShiftConvertAddC_16s8s

---
+++ src/nmplv/Select/pc/nmppsClipRShiftConvertAddC_16s8s.cpp
@@ -0,0 +1,22 @@
+#include "nmplv.h"
+extern "C" {
+void nmppsClipRShiftConvertAddC_16s8s( nm16s* pSrcVec, int nClipFactor, int nShift, int8b nAddValue,nm8s* pDstVec, int nSize)
+{
+	int i;
+	int	ClipPos=(1<<nClipFactor)-1;	
+	int ClipNeg=-ClipPos-1;
+	
+	for(i=0;i<nSize;i++,pSrcVec++,pDstVec++)
+	{
+		int x=*pSrcVec;
+		if (x>ClipPos)
+			x=ClipPos;
+		else if (x<ClipNeg)
+			x=ClipNeg;
+		x>>=nShift;
+		x+=nAddValue;
+		*pDstVec=x;
+	}
+	
+}
+};

4231420e0793cc6c168f076f70ad24b99185da25
Andrey Brodyazhenko <brodyazhenko@gmail.com>
2018-03-16 15:51:16 +0300

    malloc0,1,2,3 were deleted for nmc-gcc

--- make/src-nmc-float-gcc.mk
+++ make/src-nmc-float-gcc.mk
@@ -32,5 +32,3 @@ SRC_DIRS+= $(ROOT)/src/nmblas/level2/nm
 SRC_DIRS+= $(ROOT)/src/nmblas/level2/nmcore
 SRC_DIRS+= $(ROOT)/src/nmpls/Arithmetics/nmf-gcc
 SRC_DIRS+= $(ROOT)/src/nmplm/Init/nmf
-SRC_DIRS+= $(ROOT)/src/nmplv/Support/common/nm-gcc
-

95a61e1af04927a9c395a783f3159dbf20dca1da
Sergey Mushkaev <mushkaev@module.ru>
2018-03-20 22:04:29 +0300

    make gcc !!

--- clean.mk
+++ clean.mk
@@ -7,5 +7,5 @@ kill:
 	-$(OS_RM) *.vcproj *.sln *.vcxproj *.db output output.264
 
 clean:
-	-$(OS_RM) *.abs *.elf *.o *.ncb *.map *.elf *.suo *.user *.filters *.db .* *.opendb $(TARGET)
+	-$(OS_RM) *.abs *.elf *.o *.ncb *.map *.elf *.suo *.user *.filters *.db .* *.opendb $(TARGET) nmc4vars_win.mk
 	-$(OS_RD) Debug Release bin obj .vs
---
+++ make/nmpp-nmc3-gcc/printobj.mk
@@ -0,0 +1,5 @@
+$(info  $(OBJECTS))
+
+.PHONY:all
+all:
+	@# do nothing

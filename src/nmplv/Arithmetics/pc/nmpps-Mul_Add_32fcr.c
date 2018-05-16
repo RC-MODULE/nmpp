5f9747790a50a6855a45c5d6d13657e3fb7419e1
Ivan Zhilenkov <i.zhilenkov@module.ru>
2018-05-16 16:23:58 +0300

    added nmpps-Mul_Add_32fcr

---
+++ src/nmplv/Arithmetics/pc/nmpps-Mul_Add_32fcr.c
@@ -0,0 +1,9 @@
+#include "nmpp.h"
+
+void nmppsMul_Add_32fcr(const nm32fcr* pSrcVec1, const nm32fcr* pSrcVec2, const nm32fcr* pSrcVecAdd, nm32fcr* pDstVec, int nSize){
+	int i =0;
+	for(i = 0; i<nSize;i++){
+		pDstVec[i].im = (pSrcVec1[i].re*pSrcVec2[i].im + pSrcVec1[i].im*pSrcVec2[i].re)+pSrcVecAdd[i].im;
+		pDstVec[i].re = (pSrcVec1[i].re*pSrcVec2[i].re - pSrcVec1[i].im*pSrcVec2[i].im)+pSrcVecAdd[i].re;
+	}	
+}

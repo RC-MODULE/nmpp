0c3f4aa0f6b1c7520f779d52b443aa5c015d37bd
Alexey Birukov <les@module.ru>
2018-08-01 15:21:44 +0300

    IJK restrictions relaxing

--- include/nmblas/nmblas_sgemm.h
+++ include/nmblas/nmblas_sgemm.h
@@ -1,3 +1,4 @@
+
  /**
  *  \ingroup LEVEL3
  *  \brief     part of nmblas library, sgemm function implementation
@@ -42,6 +43,13 @@
 							"fpu 2 " instr "\n\t" \
 							"fpu 3 " instr "\n\t"
 
+
+//static inline storeToMemory()
+//{
+//
+//}
+//
+
 static inline
 void nmblas_sgemm(	const enum nm_trans TransA,
 					const enum nm_trans TransB,
@@ -54,7 +62,7 @@ void nmblas_sgemm(	const enum nm_trans TransA,
 					const float *B,
 					const int ldb,
 					const float beta,
-					volatile float *C,
+					float *C,
 					const int ldc
 					)
 {
@@ -66,21 +74,26 @@ void nmblas_sgemm(	const enum nm_trans TransA,
 	int dummy_to_link;
 
 	for(i=0; i<I; i+=32){
+		asm (
+				"vlen= %1;\n\t"
+					: "=m" (dummy_to_link)
+					: "g"( I-i-1 >= 31 ? 31 : I-i-1  ) );
+
 		for(j=0; j<J; j+=8){
 			//	read C[i][j]
-			float* pc  = ((float*)C) + i   *ldc +j;
+			float* pc  = C + i   *ldc +j;
 			asm (
 					"ar0= %1;\n\t"
 					"gr0= %2;\n\t"
-					"fpu 0 rep 32 vreg7= [ar0++gr0];\n\t"
+					"fpu 0 rep vlen vreg7= [ar0++gr0];\n\t"
 					"ar0= %1+2;\n\t"
-					"fpu 1 rep 32 vreg7= [ar0++gr0];\n\t"
+					"fpu 1 rep vlen vreg7= [ar0++gr0];\n\t"
 					"ar0= %1+4;\n\t"
-					"fpu 2 rep 32 vreg7= [ar0++gr0];\n\t"
+					"fpu 2 rep vlen vreg7= [ar0++gr0];\n\t"
 					"ar0= %1+6;\n\t"
-					"fpu 3 rep 32 vreg7= [ar0++gr0];\n\t"
-					: "=m"(dummy_to_link), "+a"(pc)
-					: "r"(ldc), "m"(*C)
+					"fpu 3 rep vlen vreg7= [ar0++gr0];\n\t"
+					: "+m"(dummy_to_link), "+a"(pc)
+					: "r"(ldc), "m"(*pc)
 						: "ar0", "gr0" );
 
 			const float* pa;
@@ -91,15 +104,16 @@ void nmblas_sgemm(	const enum nm_trans TransA,
 				pa  = A + i*lda +k;
 				pb  = B + k*ldb +j;
 				pb1 = B +(k+1)*ldb +j;
+
 				asm (
 						"ar0= %1;\n\t"
 						"gr0= %2;\n\t"
-						"fpu 0 rep 32 vreg3 = [ar0++gr0];\n\t"
+						"fpu 0 rep vlen vreg3 = [ar0++gr0];\n\t"
 						"fpu 1 vreg3 = fpu 0 vreg3;\n\t"
 						"fpu 2 vreg3 = fpu 1 vreg3;\n\t"
 						"fpu 3 vreg3 = fpu 2 vreg3;\n\t"
 							: "+m" (dummy_to_link), "+a" (pa)
-							: "r"(lda), "m"(*A)
+							: "r"(lda), "m"(*pa)
 							: "ar0", "gr0" );
 
 				asm (
@@ -111,9 +125,9 @@ void nmblas_sgemm(	const enum nm_trans TransA,
 						"fpu 2 rep 1 vreg5 = [%2++];\n\t"
 						"fpu 3 rep 1 vreg4 = [%1++];\n\t"
 						"fpu 3 rep 1 vreg5 = [%2++];\n\t"
-				      	ALL_FPU (".matrix vreg7= vreg3 * .retrive (vreg4,vreg5) + vreg7;")
+				      	ALL_FPU (".matrix vreg7= vreg3 * .retrieve (vreg4,vreg5) + vreg7;")
 							: "+m" (dummy_to_link), "+a" (pb), "+a" (pb1)
-							: "m"(*B) );
+							: "m"(*pb) );
 				k+=2;
 				if ( !( k<K ) )
 					break;
@@ -123,12 +137,12 @@ void nmblas_sgemm(	const enum nm_trans TransA,
 				asm (
 						"ar0= %1;\n\t"
 						"gr0= %2;\n\t"
-						"fpu 0 rep 32 vreg0 = [ar0++gr0];\n\t"
+						"fpu 0 rep vlen vreg0 = [ar0++gr0];\n\t"
 						"fpu 1 vreg0 = fpu 0 vreg0;\n\t"
 						"fpu 2 vreg0 = fpu 1 vreg0;\n\t"
 						"fpu 3 vreg0 = fpu 2 vreg0;\n\t"
 							: "+m" (dummy_to_link), "+a" (pa)
-							: "r"(lda), "m"(*A)
+							: "r"(lda), "m"(*pa)
 							: "ar0", "gr0" );
 
 				asm (
@@ -140,29 +154,51 @@ void nmblas_sgemm(	const enum nm_trans TransA,
 						"fpu 2 rep 1 vreg5 = [%2++];\n\t"
 						"fpu 3 rep 1 vreg4 = [%1++];\n\t"
 						"fpu 3 rep 1 vreg5 = [%2++];\n\t"
-				      	ALL_FPU (".matrix vreg7= vreg0 * .retrive (vreg4,vreg5) + vreg7;")
+				      	ALL_FPU (".matrix vreg7= vreg0 * .retrieve (vreg4,vreg5) + vreg7;")
 							: "+m" (dummy_to_link), "+a" (pb), "+a" (pb1)
-							: "m"(*B) );
+							: "m"(*pb) );
 			}
 			//	write C[i][j]
-			pc  = ((float*)C) + i   *ldc +j;
+			pc  = C + i   *ldc +j+0;
 			asm (
 					"ar0= %1;\n\t"
 					"gr0= %2;\n\t"
-					"fpu 0 rep 32 [ar0++gr0] = vreg7;\n\t"
-					"ar0= %1+2;\n\t"
-					"fpu 1 rep 32 [ar0++gr0] = vreg7;\n\t"
-					"ar0= %1+4;\n\t"
-					"fpu 2 rep 32 [ar0++gr0] = vreg7;\n\t"
-					"ar0= %1+6;\n\t"
-					"fpu 3 rep 32 [ar0++gr0] = vreg7;\n\t"
-					: "=m"(*C), "+a"(pc)
+					"fpu 0 rep vlen [ar0++gr0] = vreg7;\n\t"
+					: "=m"(*pc), "+a"(pc)
 					: "r"(ldc), "m"(dummy_to_link)
 						: "ar0", "gr0" );
-
+			if ( J-j<=2 )
+				break;
+			pc  = C + i   *ldc +j+2;
+			asm (
+					"ar0= %1;\n\t"
+					"gr0= %2;\n\t"
+					"fpu 1 rep vlen [ar0++gr0] = vreg7;\n\t"
+					: "=m"(*pc), "+a"(pc)
+					: "r"(ldc), "m"(dummy_to_link)
+						: "ar0", "gr0" );
+			if ( J-j<=4 )
+				break;
+			pc  = C + i   *ldc +j+4;
+			asm (
+					"ar0= %1;\n\t"
+					"gr0= %2;\n\t"
+					"fpu 2 rep vlen [ar0++gr0] = vreg7;\n\t"
+					: "=m"(*pc), "+a"(pc)
+					: "r"(ldc), "m"(dummy_to_link)
+						: "ar0", "gr0" );
+			if ( J-j<=6 )
+				break;
+			pc  = C + i   *ldc +j+6;
+			asm (
+					"ar0= %1;\n\t"
+					"gr0= %2;\n\t"
+					"fpu 3 rep vlen [ar0++gr0] = vreg7;\n\t"
+					: "=m"(*pc), "+a"(pc)
+					: "r"(ldc), "m"(dummy_to_link)
+						: "ar0", "gr0" );
 		}
 	}
-
 }
 
 

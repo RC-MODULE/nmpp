8e6eec4ff6f83a879fbe47a2b36100caf6928d86
Andrey Brodyazhenko <brodyazhenko@gmail.com>
2018-09-12 23:43:55 +0300

    nmppsConvert_32f32fcr ++

--- app/examples/nmppi_float/2D-FFT_mc12101/FFT256/nmppiFFT256Inv_32fcr.cpp
+++ app/examples/nmppi_float/2D-FFT_mc12101/FFT256/nmppiFFT256Inv_32fcr.cpp
@@ -5,41 +5,52 @@
 //*   Software design:  A.Brodyazhenko 																            */
 //*   Year: 2018                                                                                                */
 //*                                                                                                             */
-//* Prototype: void nmppiFFT256Inv_32fcr(const nm32fcr* pSrc, nm32fcr* pDst,									*/
-//*										 nm32fcr* bufferDDR, nm32fcr* bufferSRAM, NmppsFFTSpec_32fcr* spec) 	*/
+//* Prototype: void nmppiFFTInv_32fcr(const nm32fcr* pSrc, nm32fcr* pDst,										*/
+//*										 nm32fcr* bufferDDR, nm32fcr* bufferSRAMw, nm32fcr* bufferSRAMh,		*/						*/
+//*										 NmppsFFTSpec_32fcr* spec, int width, int height); 						*/
 //* 																								            */
-//* pSrc   		- input matrix in DDR (size = 256 * 256),														*/
-//* pDst   		- output matrix in DDR (size = 256 * 256),														*/
-//* bufferDDR   - temporary matrix in DDR (size = 256 * 256)													*/
-//*	bufferSRAM  - temporary vector in SRAM (size = 256)										            		*/
-//* spec		- struct of coeffs for 1D-FFT                              										*/
-//* Description: This function computes inverse 2D-FFT256 (floating point) from the intput matrix.				*/
-//*      It works on mc12101 only. Before start this function necessary to call halOpenDMA()! See main.cpp  	*/
+//* pSrc   		 - input matrix in DDR (size = width * height),													*/
+//* pDst   		 - output matrix in DDR (size = width * height),												*/
+//* bufferDDR    - temporary matrix in DDR (size = width * height)												*/
+//*	bufferSRAMw  - temporary vector for strings in SRAM (size = width)										    */
+//*	bufferSRAMh  - temporary vector for columns in SRAM (size = height)										    */
+//* spec		 - struct of coeffs for 1D-FFT                              									*/
+//* Description: This function computes inverse 2D-FFT (floating point) from the intput matrix.					*/
+//*     It works on mc12101 only. Before start this function necessary to call halOpenDMA()! See main.cpp       */
 //***************************************************************************************************************/
 
 #include "dma.h"
 #include "nmpp.h"
 #include "fft_32fcr.h"
 
-void nmppiFFT256Inv_32fcr(const nm32fcr* pSrc, nm32fcr* pDst, nm32fcr* bufferDDR, nm32fcr* bufferSRAM, NmppsFFTSpec_32fcr* spec)
+void nmppiFFTInv_32fcr(
+						const nm32fcr* pSrc,
+						nm32fcr* pDst,
+						nm32fcr* bufferDDR,
+						nm32fcr* bufferSRAMw,
+						nm32fcr* bufferSRAMh,
+						NmppsFFTSpec_32fcr* spec,
+						int width,
+						int height
+						)
 {
-	for(int offset = 0; offset < 256 * 256; offset += 256) {
-		halInitStatusSingleDMA((nm32fcr*)(pSrc + offset), (nm32fcr*)((int)bufferSRAM + 0x40000), 2 * 256);
+	for(int offset = 0; offset < width * height; offset += width) {
+		halInitStatusSingleDMA((nm32fcr*)(pSrc + offset), (nm32fcr*)((int)bufferSRAMw + 0x40000), 2 * width);
 		while(halStatusDMA());
 
-		nmppsFFT256Inv_32fcr(bufferSRAM, bufferSRAM, spec);
+		nmppsFFTInv_32fcr(bufferSRAMw, bufferSRAMw, spec, width);
 
-		halInitStatusSingleDMA((nm32fcr*)((int)bufferSRAM + 0x40000), (nm32fcr*)(bufferDDR + offset), 2 * 256);
+		halInitStatusSingleDMA((nm32fcr*)((int)bufferSRAMw + 0x40000), (nm32fcr*)(bufferDDR + offset), 2 * width);
 		while(halStatusDMA());
 	}
 
-	for(int height = 0, offs = 0; height < 256; height++, offs += 256) {
-		halInitMatrixDMA((nm32fcr*)(bufferDDR + height), 2, 256, 2 * 256, (nm32fcr*)((int)bufferSRAM + 0x40000), 2);
+	for(int h = 0, offs = 0; h < height; h++, offs += height) {
+		halInitMatrixDMA((nm32fcr*)(bufferDDR + h), 2, height, 2 * height, (nm32fcr*)((int)bufferSRAMh + 0x40000), 2);
 		while(halStatusDMA());
 
-		nmppsFFT256Inv_32fcr(bufferSRAM, bufferSRAM, spec);
+		nmppsFFTInv_32fcr(bufferSRAMh, bufferSRAMh, spec, height);
 
-		halInitStatusSingleDMA((nm32fcr*)((int)bufferSRAM + 0x40000), (nm32fcr*)(pDst + offs), 2 * 256);
+		halInitStatusSingleDMA((nm32fcr*)((int)bufferSRAMh + 0x40000), (nm32fcr*)(pDst + offs), 2 * height);
 		while(halStatusDMA());
 	}
 }

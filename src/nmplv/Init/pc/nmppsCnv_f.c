1e6ef3f781cca0b2ba61308aaf235a2a1398c8d4
Andrey Brodyazhenko <brodyazhenko@gmail.com>
2018-09-13 17:50:22 +0300

    nmppsConvert_32f32fcr for PC ++

--- src/nmplv/Init/pc/nmppsCnv_f.c
+++ src/nmplv/Init/pc/nmppsCnv_f.c
@@ -22,6 +22,15 @@ void nmppsConvert_32s32fcr(const nm32s* pSrcVec, nm32fcr* pDstVec, int nSize)
 	}
 }
 
+void nmppsConvert_32f32fcr(const nm32f* pSrcVec, nm32fcr* pDstVec, int nSize)
+{
+	int i;
+	for(i = 0; i < nSize; i++) {
+		pDstVec[i].re = pSrcVec[i];
+		pDstVec[i].im = 0;
+	}
+}
+
 void nmppsConvert_32sc32fcr(const nm32sc *pSrcVec, nm32fcr *pDstVec, int nSize)
 {
 	int i;

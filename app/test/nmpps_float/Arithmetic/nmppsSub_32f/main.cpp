d81764b4486f0a8053de85329320dfe06e7aa3f6
Andrey Brodyazhenko <brodyazhenko@gmail.com>
2018-12-13 15:10:21 +0300

    tests for nmppsAdd(Sub)_32f ++

--- app/test/nmpps_float/Arithmetic/nmppsSub_32f/main.cpp
+++ app/test/nmpps_float/Arithmetic/nmppsSub_32f/main.cpp
@@ -7,16 +7,21 @@ int main()
 {
 	float* src0 = (float *) nmppsMalloc_32f(1024);
 	float* src1 = (float *) nmppsMalloc_32f(1024);
-	float* dst  = (float *) nmppsMalloc_32f(1024);
+	float* dst  = (float *) nmppsMalloc_32f(1024 + 11);
 	unsigned crc=0;
 	for(int i = 0; i < 1024; i++) {
 		src0[i] = i*PI;
 		src1[i] = i+PI;
 	}
 
+	for(int i = 0; i < 1024 + 11; i++) {
+		dst[i] = 77777;
+	}
+
+
 	for(int size=2; size<1024; size+=2){
 		nmppsSub_32f(src0,src1,dst,size);
-		nmppsCrcAcc_32f(dst,0,size,&crc);
+		nmppsCrcAcc_32f(dst,0,size + 11,&crc);
 	}
 	free(src0);
 	free(src1);

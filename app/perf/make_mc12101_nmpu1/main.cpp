13a6877850e7642aefe6c2284a33ea982bce2ec3
Andrey Brodyazhenko <brodyazhenko@gmail.com>
2019-05-15 15:00:29 +0300

    Makefile for a starting testperf ++

---
+++ app/perf/make_mc12101_nmpu1/main.cpp
@@ -0,0 +1,23 @@
+#include "nmpp.h"
+#include "time.h"
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
+
+#pragma data_section ".data_imu0"
+    long long im0[2048];
+#pragma data_section ".data_imu1"
+    long long im1[2048];
+#pragma data_section ".data_imu2"
+    long long im2[2048];
+#pragma data_section ".data_imu3"
+    long long im3[2048];
+
+#pragma data_section ".data_emi"
+    long long emi[4096];
+
+
+int main()
+{
+  return 0;
+}

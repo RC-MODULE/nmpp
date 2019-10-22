7076209d2b81afc538163149157da9e46ca0d536
Sergey Mushkaev <mushkaev@module.ru>
2019-10-22 18:15:12 +0300

    12705 examples ++

--- app/examples-float/local.mk
+++ app/examples-float/local.mk
@@ -1,6 +1,6 @@
 TEMPLATE_DIR = $(ROOT)/app/templates
 #PLATFORMS =  x86 gcc mc12101_nmpu0 mc12101-gcc_nmpu0 mc12705-gcc
-PLATFORMS =  mc12705-gcc mc12705-gcc-ld mc12101_nmpu0 mc12101_nmpu0-gcc x86
+PLATFORMS =  mc12705-gcc-cfg mc12705-gcc-ld  	mc12705-gcc-cfg mc12705-gcc-ld mc12101_nmpu0 mc12101_nmpu0-gcc-cfg mc12101_nmpu0-gcc-ld
 #SUB_DIRS =  fft filter vArithmetics vBitwise vInit vSelect vStat vSupport vTransform 
 #SKIP_DIRS = rpc
 #WORK_DIRS = nmvcore nmplv nmplm nmpls nmpli nmplc  
--- clean.mk
+++ clean.mk
@@ -7,7 +7,18 @@ kill: clean
 	-$(OS_RM) *.vcproj *.sln *.vcxproj *.db output output.264
 
 clean:
-	-$(OS_RM) *.abs *.elf *.o *.ncb *.map *.elf *.suo *.user *.filters *.db *.make  .*  *.opendb  
-	-$(OS_RM) $(TARGET) nmc4vars_win.mk
+	-$(OS_RM) Release/*.o 
+	-$(OS_RM) Debug/*.o 
+	-$(OS_RM) *.abs *.elf *.ncb *.map *.elf *.suo *.user *.filters *.db *.make  .*  *.opendb  $(TARGET) nmc4vars_win.mk
 	-$(OS_RD) Debug Release bin obj .vs
-
+	
+#	$(MAKE) del_files
+#	$(MAKE) del_folders
+	
+del_files:
+	-$(OS_RM) Release/*.o 
+	-$(OS_RM) *.abs *.elf *.ncb *.map *.elf *.suo *.user *.filters *.db *.make  .*  *.opendb  $(TARGET) nmc4vars_win.mk
+	
+del_folders:
+	-$(OS_RD) Debug Release bin obj .vs
+	

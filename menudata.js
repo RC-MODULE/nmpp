68319c872cab729af0d4da391fa5920cc5382da2
Sergey Mushkaev <mushkaev@module.ru>
2020-04-23 20:48:06 +0300

    documentation-for-gh-pages

---
+++ menudata.js
@@ -0,0 +1,55 @@
+var menudata={children:[
+{text:"Титульная страница",url:"index.html"},
+{text:"Описания",url:"pages.html"},
+{text:"nmpp",url:"modules.html"},
+{text:"Классы",url:"annotated.html",children:[
+{text:"Классы",url:"annotated.html"},
+{text:"Алфавитный указатель классов",url:"classes.html"},
+{text:"Иерархия классов",url:"hierarchy.html"},
+{text:"Члены классов",url:"functions.html",children:[
+{text:"Указатель",url:"functions.html",children:[
+{text:"a",url:"functions.html#index_a"},
+{text:"c",url:"functions.html#index_c"},
+{text:"d",url:"functions.html#index_d"},
+{text:"e",url:"functions.html#index_e"},
+{text:"h",url:"functions.html#index_h"},
+{text:"i",url:"functions.html#index_i"},
+{text:"l",url:"functions.html#index_l"},
+{text:"n",url:"functions.html#index_n"},
+{text:"p",url:"functions.html#index_p"},
+{text:"r",url:"functions.html#index_r"},
+{text:"s",url:"functions.html#index_s"},
+{text:"t",url:"functions.html#index_t"},
+{text:"u",url:"functions.html#index_u"},
+{text:"w",url:"functions.html#index_w"},
+{text:"x",url:"functions.html#index_x"},
+{text:"y",url:"functions.html#index_y"}]},
+{text:"Функции",url:"functions_func.html"},
+{text:"Переменные",url:"functions_vars.html",children:[
+{text:"a",url:"functions_vars.html#index_a"},
+{text:"d",url:"functions_vars.html#index_d"},
+{text:"h",url:"functions_vars.html#index_h"},
+{text:"i",url:"functions_vars.html#index_i"},
+{text:"n",url:"functions_vars.html#index_n"},
+{text:"p",url:"functions_vars.html#index_p"},
+{text:"r",url:"functions_vars.html#index_r"},
+{text:"s",url:"functions_vars.html#index_s"},
+{text:"t",url:"functions_vars.html#index_t"},
+{text:"x",url:"functions_vars.html#index_x"},
+{text:"y",url:"functions_vars.html#index_y"}]}]}]},
+{text:"Файлы",url:"files.html",children:[
+{text:"Файлы",url:"files.html"},
+{text:"Список членов всех файлов",url:"globals.html",children:[
+{text:"Указатель",url:"globals.html",children:[
+{text:"c",url:"globals.html#index_c"},
+{text:"i",url:"globals.html#index_i"},
+{text:"n",url:"globals.html#index_n"},
+{text:"u",url:"globals.html#index_u"},
+{text:"v",url:"globals.html#index_v"}]},
+{text:"Функции",url:"globals_func.html"},
+{text:"Определения типов",url:"globals_type.html",children:[
+{text:"i",url:"globals_type.html#index_i"},
+{text:"n",url:"globals_type.html#index_n"},
+{text:"u",url:"globals_type.html#index_u"},
+{text:"v",url:"globals_type.html#index_v"}]},
+{text:"Макросы",url:"globals_defs.html"}]}]}]}

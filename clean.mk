9ef816078b64b43dffb0ebe2c90c8dea8c240119
Sergey Mushkaev <mushkaev@module.ru>
2020-12-29 17:32:10 +0000

    clean

--- clean.mk
+++ clean.mk
@@ -4,21 +4,21 @@ ifndef OS_RM
 endif
 
 kill: clean
-	-$(OS_RM) *.vcproj *.sln *.vcxproj *.db output output.264
+	-$(OS_RM) *.vcproj *.sln *.vcxproj *.db $(TARGET_LIB)
 
 clean:
 	-$(OS_RM) Release/*.o 
 	-$(OS_RM) Debug/*.o 
-	-$(OS_RM) *.abs *.elf *.ncb *.map *.elf *.suo *.user *.filters *.db *.make  .*  *.opendb  $(TARGET) nmc4vars_win.mk
+	-$(OS_RM) $(TARGET) $(TARGET)_qemu *.abs *.elf *.ncb *.map *.elf *.suo *.user *.filters *.db *.make  *.opendb .objects nmc4vars_win.mk
 	-$(OS_RD) Debug Release bin obj .vs
 	
 #	$(MAKE) del_files
 #	$(MAKE) del_folders
 	
-del_files:
-	-$(OS_RM) Release/*.o 
-	-$(OS_RM) *.abs *.elf *.ncb *.map *.elf *.suo *.user *.filters *.db *.make  .*  *.opendb  $(TARGET) nmc4vars_win.mk
+#del_files:
+#	-$(OS_RM) Release/*.o 
+#	-$(OS_RM) *.abs *.elf *.ncb *.map *.elf *.suo *.user *.filters *.db *.make  .*  *.opendb  nmc4vars_win.mk
 	
-del_folders:
-	-$(OS_RD) Debug Release bin obj .vs
+#del_folders:
+#	-$(OS_RD) Debug Release bin obj .vs
 	
